1d1a382d8305de4fab595ea74c6f8cf35fc5a07c
Evgeny Bil <aigen.chernogorsk@yandex.ru>
2024-10-17 18:48:52 +0700

    Change notification namespace

--- app/Http/Controllers/Control/Notification/NotificationController.php
+++ app/Http/Controllers/Control/Notification/NotificationController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Control\Notification;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use App\Notifications\Control\ProjectCreateNotification;
+use App\Notifications\Control\Project\ProjectCreateNotification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Notification;
 
