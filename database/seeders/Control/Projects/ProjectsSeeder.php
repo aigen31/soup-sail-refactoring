694782f7533a8646b4a58b4283d0b1a19d220753
Evgeny Bil <aigen.chernogorsk@yandex.ru>
2024-11-01 19:43:00 +0700

    Add assistant relationships

--- database/seeders/Control/Projects/ProjectsSeeder.php
+++ database/seeders/Control/Projects/ProjectsSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders\Control\Projects;
 
+use App\Http\Controllers\Control\Role\RoleController;
 use App\Models\Control\Project;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class ProjectsSeeder extends Seeder
@@ -12,6 +14,12 @@ class ProjectsSeeder extends Seeder
    */
   public function run(): void
   {
-    Project::factory()->count(10)->create();
+    $assistants = User::where('role_id', RoleController::getBySlug('moderator'))->get();
+
+    Project::factory()->count(10)->create()->each(function (Project $project) use ($assistants) {
+      $project->assistants()->attach(
+        $assistants->random(1)->pluck('id')
+      );
+    });
   }
 }
